83da82228824fc0b0fa1beeb4373688dad088b07
Srabutdotcom <handoko75@gmail.com>
2021-03-24 00:28:50 +0700

    change to async func

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const pdfjs = require("pdfjs-dist");
 pdfjs.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.6.347/build/pdf.worker.js";
 const parsepdf = require('./library/parsepdf.js');
 
-function loadPdf(pdfUrl,pageNum){
+async function loadPdf(pdfUrl,pageNum){
 	// Asynchronous download of PDF
 	let loadingTask = await pdfjs.getDocument(pdfUrl);
 	loadingTask.promise.then(function(pdf) {
